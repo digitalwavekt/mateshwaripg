1ffa47f4317dc32e35581438a637af3916d6b683
yogesh kumar saini <105904333+Ktkmina@users.noreply.github.com>
2024-12-23 11:21:02 +0530

    fbuild

--- components/Map.tsx
+++ components/Map.tsx
@@ -4,7 +4,7 @@ const Map = () => {
       <h2 className="text-4xl font-bold text-center mb-12">Location</h2>
       <div className="aspect-video rounded-lg overflow-hidden shadow-lg">
         <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5391.081886300955!2d75.73205090000002!3d26.935135800000012!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x396db32314bc2c29%3A0x42049a2b65f8726e!2sMateshwari%20Boys&#39;%20PG!5e1!3m2!1sen!2sin!4v1734610505135!5m2!1sen!2sin" width="800" height="600" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5391.081886300955!2d75.73205090000002!3d26.935135800000012!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x396db32314bc2c29%3A0x42049a2b65f8726e!2sMateshwari%20Boys&#39;%20PG!5e1!3m2!1sen!2sin!4v1734610505135!5m2!1sen!2sin" 
           width="100%"
           height="100%"
           style={{ border: 0 }}
